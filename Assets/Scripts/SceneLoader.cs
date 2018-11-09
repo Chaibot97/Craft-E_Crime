de7dcdad411a00749299fc24e51e09211697490b
Raegan <Raegan1414@gmail.com>
2018-11-09 02:17:08 -0500

    Created prefabs(with textures and mesh colliders) and organized files into folders

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadScene(String level)
+    {
+        if (level != "Main")
+            Cursor.visible = true;
+        else
+            Cursor.visible = false;
+        SceneManager.LoadScene(level);
+    }
+}
